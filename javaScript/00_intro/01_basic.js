150bd145c2a5ebbc289bf21858fd81f0fa3ba144
ibad <ibadkhan476928208@gmail.com>
2026-08-29 10:05:34 +0500

    docs: add notes topic

---
+++ javaScript/00_intro/01_basic.js
@@ -0,0 +1,9 @@
+/*
+console.table() is a console method that displays arrays and objects in a table format, making them much easier to read.
+*/
+
+const pi = 3.14;
+let myName = 'ibad';
+var age = 21;
+
+console.table([pi, myName, age])
