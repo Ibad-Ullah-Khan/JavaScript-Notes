cecdfd85ff94fba860effe390f435f1026a60564
ibad <ibadkhan476928208@gmail.com>
2026-08-29 10:09:54 +0500

    docs: Add prefix and postfix file

---
+++ javaScript/02_string/03_prefix_postfix.js
@@ -0,0 +1,15 @@
+/*
+1. Prefix (++x)
+The variable is updated first, then its value is used.
+*/
+
+let x = 5;
+console.log(++x);
+
+/*
+2. Postfix (x++)
+The current value is used first, then the variable is updated.
+*/
+
+let y = 5;
+console.log(y++);
