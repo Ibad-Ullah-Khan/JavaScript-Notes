0913a064500c31fbdea3b537403e430e5b94271a
ibad <ibadkhan476928208@gmail.com>
2026-09-02 11:33:19 +0500

    docs: add Math methods file

---
+++ javaScript/03_number/02_Maths.js
@@ -0,0 +1,14 @@
+// Math.abs() returns the absolute value of a number.
+console.log(Math.abs(-19))
+
+// Math.round() rounds a number to the nearest integer.
+console.log(Math.round(4.23))
+
+// minimum value
+console.log(Math.min(1,2,3,4,0))
+
+// maximum value
+console.log(Math.max(1,2,3,4,5,6))
+
+// generate random values between 0 and 1
+console.log(Math.random())
